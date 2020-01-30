326b85a0d9849d4064d6763f2876c5b4388d0613
Roudi Korkis Kanaan <roudi@newsperform.com.au>
2020-01-31 10:26:39 +1100

    Refactor extension

--- app/src/main/java/com/example/usecasetest/ViewModelExtensions.kt
+++ app/src/main/java/com/example/usecasetest/ViewModelExtensions.kt
@@ -4,18 +4,21 @@ import androidx.lifecycle.MutableLiveData
 
 
 fun <T, Params> BaseUseCase<T, Params>.loadWithLiveData(viewStateLiveData: MutableLiveData<ViewState<T>>): BaseUseCase<T, Params> {
-    doBefore { viewStateLiveData.value = ViewState.Loading() }
-        .onResult {
-            viewStateLiveData.value = ViewState.Result(it)
-        }
-        .onCancel {
-            viewStateLiveData.value = ViewState.Cancelled()
-        }
-        .onError {
-            viewStateLiveData.value = ViewState.Error(it)
-        }
-        .doAfter {
-            viewStateLiveData.value = ViewState.Success((viewStateLiveData.value as? ViewState.Result?)?.result)
-        }
+    doBefore {
+        viewStateLiveData.value = ViewState.Loading()
+    }
+    onResult {
+        viewStateLiveData.value = ViewState.Result(it)
+    }
+    onCancel {
+        viewStateLiveData.value = ViewState.Cancelled()
+    }
+    onError {
+        viewStateLiveData.value = ViewState.Error(it)
+    }
+    doAfter {
+        val result = (viewStateLiveData.value as? ViewState.Result?)?.result
+        viewStateLiveData.value = ViewState.Success(result)
+    }
     return this
 }
